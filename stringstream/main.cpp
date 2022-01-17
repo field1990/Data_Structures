0eef8068c13de93dde97a20a4d589bb1324a100d
Matthew Field <W0450913@campus.nscc.ca>
2022-01-17 13:43:11 -0400

    Simple stringstream example

---
+++ stringstream/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    char command;
+    int start, end;
+
+    cout << "Enter a command and two numbers: " << endl;
+    cin >> command >> start >> end;
+
+    cout << "command: " << command;
+    cout << ", start: " << start;
+    cout << ", end: " << end << endl;
+
+
+    return 0;
+}
